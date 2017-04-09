be7e3287c3bde2e87ecc5a3657da6a59ac18f8ed
Pedro Vinicius <sistema@visatel.com.br>
2017-04-08 23:24:57 -0300

    Commit inicial dos testes de JWT em Node.js

---
+++ users.js
@@ -0,0 +1,6 @@
+var users = [
+{id: 1, name:"Pedro",email:"pedro.vferreira@outlook.com",password:"123456"},
+	{id:2, name:"Isabela",email:"isa@outlook.com",password:"123456"}
+];
+
+module.exports = users;
